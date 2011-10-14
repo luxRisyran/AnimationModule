788c72234d09a626d74607fc648b0ab2bf9875ef
Lux <Lux.Risyran@gmail.com>
2011-10-14 16:01:02 +0800

    first commit

---
+++ AnimationModule/AnimationController.h
@@ -0,0 +1,13 @@
+//
+//  AnimationController.h
+//  AnimationModule
+//
+//  Created by 溪 陆 on 11-10-14.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AnimationController : NSObject
+
+@end
---
+++ AnimationModule/AnimationController.m
@@ -0,0 +1,23 @@
+//
+//  AnimationController.m
+//  AnimationModule
+//
+//  Created by 溪 陆 on 11-10-14.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AnimationController.h"
+
+@implementation AnimationController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
