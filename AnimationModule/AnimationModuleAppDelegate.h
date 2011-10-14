816d94d9fbfb6ea688bd7b0a3116103c7ec26811
lux <luxRisyran@gmal.com>
2011-10-14 12:14:46 +0800

    Initial Commit

---
+++ AnimationModule/AnimationModuleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AnimationModuleAppDelegate.h
+//  AnimationModule
+//
+//  Created by 溪 陆 on 11-10-14.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnimationModuleAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ AnimationModule/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AnimationModule
+//
+//  Created by 溪 陆 on 11-10-14.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
